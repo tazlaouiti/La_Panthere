22a393826383fd7973130d3a406ca2fe2bdbf63c
tazlaouiti <mlaouiti@gmail.com>
2023-01-22 09:05:23 +0100

    Version 01 : 20230122 - First full version on github

---
+++ includes/form_1.php
@@ -0,0 +1,24 @@
+<?php	
+	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	{
+		return false;
+	}
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$input_504 = $_POST['input_504'];
+	$message = $_POST['message'];
+	
+	$to = 'contact@agencelapanthere.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from your website : La Panthère.";
+	$email_body = "You have received a new message. \n\n".
+				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: contact@agencelapanthere.com\n";
+	$headers .= "Reply-To: $input_504";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
